91e2411f7ee52060c09bb0db41bdb9c89e5496d9
CoderVinit <vinitpatel8896@gmail.com>
2024-09-28 14:42:57 +0530

    url change

--- app.js
+++ app.js
@@ -54,6 +54,13 @@ app.use(cookieParser())
 const adminSecretKey = process.env.ADMIN_SECRET_KEY || "dadjasgdugwufeJKdf"
 const userSocketIDs = new Map()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cors(corsOptions))
 
 app.use("/api/v1/users", userRoutes)
