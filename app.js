66f8717c7b55d57d6b392f08d0d8c2da24a4c164
CoderVinit <vinitpatel8896@gmail.com>
2024-09-28 14:50:22 +0530

    cors

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
+app.options("*", cors(corsOptions))
 app.use(cors(corsOptions))
 
 app.use("/api/v1/users", userRoutes)
