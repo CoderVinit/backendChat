002d51fa172c37ed2c396031ebd4e6bbec5ed562
CoderVinit <vinitpatel8896@gmail.com>
2024-09-29 20:06:51 +0530

    cors

--- app.js
+++ app.js
@@ -60,7 +60,25 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-app.use(cors(corsOptions))
+
+const allowedOrigins = [
+  "https://chat-app-wft1.vercel.app", "http://localhost:5173", process.env.CLIENT_URL
+]
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // Allow credentials
+}));
 app.options("*", cors(corsOptions));
 
 app.use("/api/v1/users", userRoutes)
