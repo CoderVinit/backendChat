365160ac90d7340c3b6e26e424663157c7e07997
CoderVinit <vinitpatel8896@gmail.com>
2024-09-29 20:53:52 +0530

    cors

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,6 @@
 
 const corsOptions = {
-  origin: ["https://chat-app-wft1.vercel.app", "http://localhost:5173", process.env.CLIENT_URL],
+  origin: ["https://chat-app-ten-ecru-78.vercel.app", "http://localhost:5173", process.env.CLIENT_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }
